438ef14aa91f2f023b1f377bbcf8dc3bfb39812c
codepleb <codepleb4@gmail.com>
2017-10-20 12:31:07 -0700

    Push current broken state

--- src/main/java/ApplicationEntry.java
+++
@@ -1,9 +0,0 @@
-@PropertySources({@PropertySource(value = "classpath:application.properties")})
-@SpringBootApplication
-public class ApplicationEntry {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationEntry.class, args);
-    }
-
-}
